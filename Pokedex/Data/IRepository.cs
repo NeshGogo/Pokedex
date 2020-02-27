7f4b142bb8f4929544a645f92cefb7dcd7456fb0
NeshGogo <rafaelaguero10@hotmail.com>
2020-02-27 17:36:33 -0400

    Creando respositorio generico
    Se creo toda la estructura que para el repositorio generico de la aplicacion

---
+++ Pokedex/Data/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokedex.Data
+{
+    interface IRepository<T> where T: class
+    {
+       public Task<IEnumerable<T>> GetAll();
+       public Task<T> GetById(int id);
+       public Task<T> Insert(T entity);
+       public Task<IEnumerable<T>> InsertRage(IEnumerable<T> entities);
+       public Task<T> Update(T entity);
+       public Task<IEnumerable<T>> UpdateRange(IEnumerable<T> entities);
+
+       public Task<bool> Delete(T entity);
+       public Task<bool> DeleteRange(IEnumerable<T> entities);
+
+    }
+}
---
+++ Pokedex/Data/SqlRepository.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokedex.Data
+{
+    public class SqlRepository<T> : IRepository<T> where T : class
+    {
+        private readonly PokedexDBContext _context;
+        private DbSet<T> _entities;
+        public virtual DbSet<T> Entities 
+        {
+            get
+            {
+                if (_entities == null)
+                    _entities = _context.Set<T>();
+
+                return _entities;
+            }
+        }
+
+        public SqlRepository(PokedexDBContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            try
+            {
+                return await Entities.ToListAsync();
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            try
+            {
+                return await Entities.FindAsync(id);
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+        public  async Task<T> Insert(T entity)
+        {
+            try
+            {
+                await Entities.AddAsync(entity);
+                await _context.SaveChangesAsync();
+                return entity;
+            }
+            catch (Exception)
+            {
+                return null;
+            }                   
+        }
+
+        public async Task<IEnumerable<T>> InsertRage(IEnumerable<T> entities)
+        {
+            try
+            {
+                await Entities.AddRangeAsync(entities);
+                await _context.SaveChangesAsync();
+                return entities;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            try
+            {
+                Entities.Update(entity);
+                await _context.SaveChangesAsync();
+                return entity;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<IEnumerable<T>> UpdateRange(IEnumerable<T> entities)
+        {
+            try
+            {
+                Entities.UpdateRange(entities);
+                await _context.SaveChangesAsync();
+                return entities;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public async Task<bool> Delete(T entity)
+        {
+            try
+            {
+                Entities.Remove(entity);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+
+        }
+
+        public async Task<bool> DeleteRange(IEnumerable<T> entities)
+        {
+            try
+            {
+                Entities.RemoveRange(entities);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
